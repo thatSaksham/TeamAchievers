b1aac1e20f953f5f51785d3401bc63b8f2b4e78a
Bishal Sarkar <bishalsarkarsagarnil2004@gmail.com>
2024-07-09 16:35:03 +0530

    footer created

--- nielseneats/src/components/Footer/Footer.jsx
+++ nielseneats/src/components/Footer/Footer.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
 
-function Footer() {
-    return (
-        <div>
-            
+
+const Footer=()=> {
+  return (
+    
+
+   <div className='footer' id='footer'>
+      <div className='footer-content'>
+        <div className="footer-content-left">
+            <img className="footer-logo"src={assets.logo} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempore, cum reprehenderit? Cupiditate, pariatur? Praesentium odio quidem id dolor adipisci nesciunt doloremque sed vitae, accusantium distinctio ipsa, similique natus ut quisquam.</p>
+            <div className="footer-social-icons">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+            </div>
         </div>
-    );
+
+        <div className="footer-content-center">
+            <h2>Company</h2>
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+
+        </div> 
+
+        <div className="footer-content-right">
+             <h2>Get in touch</h2>
+             <ul>
+                <li>+91 xxxxxxxxxx</li>
+                <li>achievers@gmail.com</li>
+             </ul>
+        </div>
+      </div>
+      <hr/>
+      <p className="footer-copyright">Copyright © 2024 NIELSEN EATS. All rights reserved.
+
+
+
+
+
+</p>
+   </div>   
+   
+  )
 }
 
-export default Footer;
+export default Footer
