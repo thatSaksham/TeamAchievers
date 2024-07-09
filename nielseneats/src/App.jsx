318062a915123e956aff3d77241ea9af0e561de4
Manav-Shandilya <manavshandilya2004@gmail.com>
2024-07-09 15:40:08 +0530

    Updated App.jsx

--- nielseneats/src/App.jsx
+++ nielseneats/src/App.jsx
@@ -1,13 +1,35 @@
-import './App.css'
-import { RouterProvider } from "react-router-dom";
-import router from "./router/Router.jsx";
+import React, { useState } from 'react'
+import NavBar from './components/NavBar/NavBar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+import Footer from './components/Footer/Footer'
+import LoginPopUp from './components/LoginPopUp/LoginPopUp'
 
-function App() {
+const App = () => {
+
+  const [showLogin,setShowLogin]=useState(false)
 
   return (
     <>
-      <RouterProvider router={router} />
+
+    {showLogin?<LoginPopUp setShowLogin={setShowLogin}/>:<></>}
+    
+    <div className='app'>
+      <NavBar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/order' element={<PlaceOrder/>} />
+
+      </Routes>
+    </div>
+
+    <Footer/>
+
     </>
+   
   )
 }
 
