3a91b1b4d40505b6ba720f0146167e87ca4a0a44
Manav-Shandilya <manavshandilya2004@gmail.com>
2024-07-09 16:03:34 +0530

    created exploreMenu component

--- nielseneats/src/components/ExploreMenu/ExploreMenu.jsx
+++ nielseneats/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from 'react'
+import './ExploreMenu.css'
+import { menu_list } from '../../assets/assets'
 
-function ExploreMenu() {
+const ExploreMenu=({category,setCategory})=> {
   return (
-    <div>
+    <div className='explore-menu' id='explore-menu'>
+        <h1>Explore our menu</h1>
+
+        <p className='explore-menu-text'>Dive into a world of culinary innovation with our chef's special creations, showcasing the finest seasonal ingredients sourced locally and globally.</p>
+         
+         <div className="explore-menu-list">
+            {menu_list.map((item,index)=>{
+              return (
+              <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index}
+               className="explore-menu-list-item">
+               <img className={category===item.menu_name?"active":""} src={item.menu_image}/>
+               <p>{item.menu_name}</p>
+               </div>  
+               )  
+            })}
+         </div>
+         
+         <hr />
       
     </div>
-  );
+  )
 }
 
-export default ExploreMenu;
+export default ExploreMenu
