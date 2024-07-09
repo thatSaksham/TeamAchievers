15dfdf4258fafd1119de9f86d15e4298e612be58
thatSaksham <saksham6001.be21@chitkara.edu.in>
2024-07-09 13:49:40 +0530

    setup router and initialized some more pages

--- nielseneats/src/App.jsx
+++ nielseneats/src/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
 import './App.css'
+import { RouterProvider } from "react-router-dom";
+import router from "./router/Router.jsx";
 
 function App() {
 
   return (
     <>
-    
+      <RouterProvider router={router} />
     </>
   )
 }
