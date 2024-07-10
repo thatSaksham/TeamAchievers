5b3f40244eb49565163150791f7b245067ed3e09
Manav-Shandilya <manavshandilya2004@gmail.com>
2024-07-10 12:38:21 +0530

    added hashlink property to smoothly transition

--- nielseneats/src/components/Header/Header.jsx
+++ nielseneats/src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import './header.css'
+import './header.css';
+import { HashLink } from 'react-router-hash-link';
 
 function Header() {
     return (
         <div className='header'>
             <div className='header-contents'>
                 <h2>Order your Favorite food here</h2>
-                <p>Choose from a wide range of menu with finger licking tastes and also exciting offer </p>
-                <button className='header-cursor' >View Menu</button>
+                <p>Choose from a wide range of menu with finger licking tastes and also exciting offers</p>
+                <HashLink smooth to="#food-display">
+                    <button className='header-cursor'>View Menu</button>
+                </HashLink>
             </div>
         </div>
     );
